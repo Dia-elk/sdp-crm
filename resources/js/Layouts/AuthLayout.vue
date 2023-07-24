759dd3543c928299ed7b46b3c22c3d43a7030304
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-07-24 21:47:29 +0100

    Login

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex w-full h-screen">
+        <!-- form section -->
+        <div class="w-full flex flex-col justify-between p-6 sm:p-10">
+            <!-- logo -->
+            <div class="">
+                <Logo/>
+            </div>
+            <!-- form  -->
+                <slot/>
+            <!-- footer-->
+            <div class="flex justify-center">
+                <p class="text-neutral-400 text-[10px]">
+                    Copyright © {{ currentYear }} | Created by
+                    <span class="text-orange-400 font-bold text-xs">
+                        <a href="https://diaelk.dev/" target="_blank">diaelk.dev</a>
+                    </span>
+                </p>
+            </div>
+        </div>
+        <!-- end of form section -->
+
+        <!-- picture section -->
+        <div class="w-fit hidden lg:flex  justify-end items-center  p-2">
+            <img src="images/loginBg.jpg" class="h-[calc(100vh-10px)]  hidden md:block w-[900px] object-cover">
+        </div>
+        <!-- end of picture section -->
+    </div>
+</template>
+
+<script setup>
+import Logo from "@/Icons/Logo.vue";
+let currentYear = new Date().getFullYear();
+
+</script>
+
---
+++ resources/js/Shared/Button.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <div class="w-[300px] h-[46px] cursor-pointer hover:bg-blue-700 transition-all duration-300 flex justify-center items-center bg-blue-600 rounded-lg">
+        <p class=" text-white text-sm  font-semibold">
+            <slot/>
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
