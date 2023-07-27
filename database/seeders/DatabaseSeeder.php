71de29c756b93fa8b9f44de452485bd79d5c37f1
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-07-27 15:42:08 +0100

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-      
+
     }
 }
